580447ce80e2fd8f726c148032f52597e6ee4237
Marc Chéhab <marc@luzmedia.ch>
2025-07-08 18:33:00 +0200

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  serverExternalPackages: ['@prisma/client']
-};
-
-export default nextConfig;
+  /* config options here */
+}
+ 
+export default nextConfig
